45d3a8534b78629ef2b31178093cb1b859a7c59e
cjheppell <chris.john.heppell@gmail.com>
2022-01-07 13:46:51 +0000

    add github package for listing all tags

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/cjheppell/snyk-history-scanner
 go 1.15
 
 require (
+	github.com/google/go-github/v39 v39.2.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.3
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
 )
---
+++ pkg/github/client.go
@@ -0,0 +1,69 @@
+package github
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/go-github/v39/github"
+	"golang.org/x/oauth2"
+)
+
+type GithubClient struct {
+	client *github.Client
+}
+
+func NewClient(token string) (*GithubClient, error) {
+	var client *github.Client
+	if token != "" {
+		ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+		tc := oauth2.NewClient(context.TODO(), ts)
+		client = github.NewClient(tc)
+	} else {
+		client = github.NewClient(http.DefaultClient)
+	}
+
+	return &GithubClient{
+		client: client,
+	}, nil
+}
+
+type tag struct {
+	Name   string `json:"name"`
+	Commit commit `json:"commit"`
+}
+
+type commit struct {
+	SHA string `json:"sha"`
+}
+
+func (c GithubClient) ListTags(owner, repoName string) ([]tag, error) {
+	listOpts := &github.ListOptions{
+		PerPage: 30,
+	}
+
+	var allTags []*github.RepositoryTag
+
+	for {
+		tags, resp, err := c.client.Repositories.ListTags(context.TODO(), owner, repoName, listOpts)
+		if err != nil {
+			return nil, err
+		}
+		allTags = append(allTags, tags...)
+		if resp.NextPage == 0 {
+			break
+		}
+		listOpts.Page = resp.NextPage
+	}
+
+	var tags []tag
+	for _, t := range allTags {
+		tags = append(tags, tag{
+			Name: *t.Name,
+			Commit: commit{
+				SHA: *t.Commit.SHA,
+			},
+		})
+	}
+
+	return tags, nil
+}
